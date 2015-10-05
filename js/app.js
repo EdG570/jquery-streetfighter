8b632b034f320a6c74f0ffa18986ac84647829d6
Ed Glazenski <edglazenski@gmail.com>
2015-10-05 17:07:34 -0400

    Added hadouken mp3

--- js/app.js
+++ js/app.js
@@ -11,15 +11,29 @@ $(document).ready(function(){
     })
 
     .mousedown(function(){
-      //play hadouken sound
+      playHadouken();
       $('.ryu-ready').hide();
       $('.ryu-throwing').show();
-      $('.hadouken').show();
-      //animate hadouken to the right of screen
+      $('.hadouken').finish().show().animate(
+        {'left': '1020px'},
+        500,
+        function() {
+          $(this).hide();
+          $(this).css('left', '600px');
+        }
+      );
+     
     })
 
     .mouseup(function(){
       $('.ryu-throwing').hide();
       $('.ryu-ready').show();
     });
+
+    function playHadouken() {
+      $('#hadouken-sound')[0].volume=0.5;
+      $('#hadouken-sound')[0].load();
+      $('#hadouken-sound')[0].play();
+    };
+
 });
